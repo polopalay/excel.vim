c46866adde1caec2737ac963f9694cc66236b64e
polopalay <60103759+polopalay@users.noreply.github.com>
2026-06-24 16:36:15 +0700

    clean build version

--- rs/target/release/build/crc32fast-a71e26d525c82e27/build_script_build-a71e26d525c82e27.d
+++
@@ -1,5 +0,0 @@
-/Users/Mac/Documents/Git/excel.vim.2/rs/target/release/build/crc32fast-a71e26d525c82e27/build_script_build-a71e26d525c82e27.d: /Users/Mac/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crc32fast-1.5.0/build.rs
-
-/Users/Mac/Documents/Git/excel.vim.2/rs/target/release/build/crc32fast-a71e26d525c82e27/build_script_build-a71e26d525c82e27: /Users/Mac/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crc32fast-1.5.0/build.rs
-
-/Users/Mac/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crc32fast-1.5.0/build.rs:
